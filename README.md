b9693d39731d721a66bd8260990a3686ad51ec55
fazetu <tfazendin17@gmail.com>
2022-09-15 18:05:08 -0500

    add to readme. add script

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # press
-Automate pressing a key using powershell
+
+Automate pressing a key using powershell.
+
+## Setup
+
+1. Copy the contents of the file in this repository called [`press.ps1`](press.ps1) to your clipboard.
+1. Open powershell.
+1. Run `notepad $PROFILE`.
+1. At the bottom of the file that opened paste what you copied in step 1.
+1. Save that file and close it.
+1. Close powershell.
+1. To check that setup worked open a new powershell (you must close and reopen powershell in order for that profile that we edited to get run) and run `Get-Help press -Detailed`. You should see help information.
+
+## Usage
+
+1. Open powershell.
+1. Run `press`.
+1. Minimize powershell and go on with your day.
+1. When finished, hold `ctrl` and press `c` (`ctrl+c`).
+
+### Custom Usage
+
+Any key can be pressed at any interval. For example `press a 5` will press the "a" key every 5 seconds.
+
+## How to Get Help Information.
+
+1. Open powershell.
+1. Run `Get-Help press -Detailed`.
---
+++ press.ps1
@@ -0,0 +1,55 @@
+function press {
+	<#
+		.SYNOPSIS
+		Press a key while repeatedly at a given interval on the current active application.
+
+		.DESCRIPTION
+		Press the specified repeatedly at the specified interval. Quit out of the repetition by pressing ctrl+c.
+
+		.PARAMETER key
+		The key to press repeatedly. Defaults to the f15 key ("F15").
+
+		.PARAMETER interval
+		The interval (in seconds) to wait before pressing the key again. Defaults to 300 seconds or 5 minutes.
+
+		.EXAMPLE
+		PS> press
+
+        .EXAMPLE
+		PS> press F15
+
+        .EXAMPLE
+		PS> press F15 300
+
+        .EXAMPLE
+		PS> press -key F15 -interval 300
+	#>
+	param (
+		[Parameter()][string] $key = "F15",
+		[Parameter()][int] $interval = 300
+	)
+	$start_time = Get-Date
+	Write-Output "Press ctrl+c to quit."
+	$times = 0
+	$wshell = New-Object -ComObject wscript.shell;
+
+	try {
+		while ($true) {
+			$wshell.SendKeys("{$key}")
+			$times++
+			Start-Sleep -Seconds $interval
+		}	
+	}
+	finally {
+		$end_time = Get-Date
+		$elapsed = $end_time - $start_time
+
+		if ($times -gt 1) {
+			$plural = "s"
+		} else {
+			$plural = ""
+		}
+
+		Write-Host "Key was pressed $times time$plural. Ran for $elapsed."
+	}
+}
